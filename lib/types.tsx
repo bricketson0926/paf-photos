4c62afd6316f7f5e5054013b4f0d52fb980f4cd0
AustinC02 <austin.m.carrington@gmail.com>
2026-05-07 13:34:21 -0500

    Refactor AdminNavbar to remove 'Add Audio Recording' link and update AddPhoto component to handle tags as checkboxes with possibleTags
    Also added the AddMap component, as well as the corresponding types

--- lib/types.tsx
+++ lib/types.tsx
@@ -7,3 +7,12 @@ export type Photo = {
     dateTaken: string; // ISO date string
     tags: string[];
 }
+
+export type Map = {
+    id: number;
+    url: string; // Will potentially be a different way to store the map
+    title: string;
+    description: string;
+    creator: string; // Won't be shown for maps, but good to have for data consistency
+    dateCreated: string; // ISO date string
+}
