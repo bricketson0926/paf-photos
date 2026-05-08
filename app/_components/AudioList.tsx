77f43cf394e4d7c4353f8ae80e9a9b2acca8d947
Blake Ricketson <ricketsonblake@gmail.com>
2026-05-07 21:21:52 -0500

    Consolidated types, got an audio clip to play.

--- app/_components/AudioList.tsx
+++ app/_components/AudioList.tsx
@@ -1,7 +1,8 @@
-import {AudioClipListItem} from "@/types";
+import {AudioClip} from "@/types";
 
-export default function AudioList({clips}: {clips: AudioClipListItem[] }) {
-    if (!clips || clips.length == 0) {
+
+export default function AudioList({clips}: {clips: AudioClip[] }) {
+    if (!clips || clips[0]['duration'] == -1) {
         return (
             <div >
                 No Audio Clips Found
@@ -9,13 +10,19 @@ export default function AudioList({clips}: {clips: AudioClipListItem[] }) {
         )
     }
 
+    console.log("Clips: " + JSON.stringify(clips))
+    console.log("URL to Pull from: " + process.env.CLOUDFRONT_URL + clips[0]['title'] + '.m4a')
+
     return (
         <div>
             {clips.map((clip) => (
                 <div key={clip.key}>
                     <h2>{clip.title}</h2>
-
-                    
+                    <div>
+                        <audio controls src={process.env.CLOUDFRONT_URL + clip.title + '.m4a'}>
+                            Your browser does not support the audio element.
+                        </audio>
+                    </div>
                 </div>
             ))}
         </div>
--- lib/getAudioClipList.ts
+++ lib/getAudioClipList.ts
@@ -1,30 +1,23 @@
-import { AudioClipListItem } from "@/types";
+import { AudioClip } from "@/types";
 
-export async function getAudioClipList(): Promise<AudioClipListItem[]> {
+export async function getAudioClipList(): Promise<AudioClip[]> {
 
-    
     //Pull info from CloudFront if it's there
-    // try {
-    //     const cloudFrontResponse = await fetch(process.env.CLOUDFRONT_URL + )
+    try {
+        const cloudFrontResponse = await fetch(process.env.CLOUDFRONT_URL + "audio-metadata.json")
 
-    //     //If it was there, return it
-    //     if (cloudFrontResponse.ok) {
-    //         return await cloudFrontResponse.json();
-    //     } 
-    // } 
-    // catch (error) {
-    //     console.log("CloudFront failed to retrieve data.")
-    // }
+        const audioClips = await cloudFrontResponse.json()
+        for(let i = 0; i<audioClips.length; i++) {
+            console.log("Clip: " + JSON.stringify(audioClips[i]))
+            audioClips[i]['duration'] = Number.parseInt(audioClips[i]['duration'])
+        }
 
-    //If it isn't in CloudFront, pull from DB
-    const apiResponse = await fetch(process.env.API_URL + "getAudioClipList", {
-        method: "GET",
-        cache: "no-store"
-    });
-
-    if (!apiResponse.ok) {
-        throw new Error("APIGateway failed to retrieve Audio metadata.")
+        console.log("audioClips: " + JSON.stringify(audioClips))
+        return audioClips['audioClips'];
+    } 
+    catch (error) {
+        console.log("Error: " + error)
+        console.log("CloudFront failed to retrieve data.")
+        return [{'title' : 'failed retrieving metadata', 'key' : '-1', 'duration' : -1, 'url' : 'no'}];
     }
-
-    return apiResponse.json();
 }
