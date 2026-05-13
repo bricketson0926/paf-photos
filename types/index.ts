14baad19ceaff08fdd7a5e68687aebf9960160ff
Blake Ricketson <ricketsonblake@gmail.com>
2026-05-12 23:53:36 -0500

    Updated file types in types/index.ts

--- types/index.ts
+++ types/index.ts
@@ -8,17 +8,15 @@ export type AudioClip = {
 
 export type Photo = {
     id: string;
-    url: string; // Will be generated based on the ID and extension, so it can be a string in the database but will be transformed into a full URL when retrieved.
     ext: string; // file extension
     title: string;
     description: string;
-    photographer: string;
+    dateTaken: string; // ISO date string
     tags: string[];
 }
 
 export type Map = {
-    id: number;
-    url: string; // Will potentially be a different way to store the map
+    id: string;
     title: string;
     description: string;
     creator: string; // Won't be shown for maps, but good to have for data consistency
